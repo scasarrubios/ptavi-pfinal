95210dc9b7c41e3a630eeb898760d09717f971b2
Sergio Casarrubios <scasarrubios@virtualbox.org>
2017-01-03 04:07:13 +0100

    Añadido el manejo de mensajes invite y conexión entre UAClient y UAServer

--- uaclient.py
+++ uaclient.py
@@ -29,24 +29,39 @@ if __name__ == "__main__":
 
     # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    #my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((config_data['regproxy']['ip'],
                        int(config_data['regproxy']['puerto'])))
     if METHOD == 'REGISTER':
-        line = METHOD + ' sip:' + str(config_data['account']['username']) + ':' + \
-         str(config_data['uaserver']['puerto']) + ' SIP/2.0 Expires: ' + OPTION
-    print("Enviando: " + line)
-    my_socket.send(bytes(line, 'utf-8') + b'\r\n\r\n')
-    answer = my_socket.recv(1024).decode('utf-8')
-    if answer.split()[1] == '401':
-        nonce = answer.split('"')[1]
-        response = hashlib.sha1()
-        response.update(bytes(config_data['account']['passwd'],'utf-8'))
-        response.update(bytes(nonce,'utf-8'))
-        response = response.hexdigest()
-        authorization = 'Authorization: Digest response="' + response \
-                        + '"\r\n\r\n'
-        line += '\r\n' + authorization
+        line = METHOD + ' sip:' + str(config_data['account']['username']) + \
+            ':' + str(config_data['uaserver']['puerto']) + \
+            ' SIP/2.0 Expires: ' + OPTION
+        print("Enviando: " + line)
+        my_socket.send(bytes(line, 'utf-8') + b'\r\n\r\n')
+        answer = my_socket.recv(1024).decode('utf-8')
+
+        # Realizamos la autenticación
+        if answer.split()[1] == '401':
+            nonce = answer.split('"')[1]
+            response = hashlib.sha1()
+            response.update(bytes(config_data['account']['passwd'], 'utf-8'))
+            response.update(bytes(nonce, 'utf-8'))
+            response = response.hexdigest()
+            authorization = 'Authorization: Digest response="' + response
+            line += '\r\n' + authorization
+            my_socket.send(bytes(line, 'utf-8') + b'\r\n\r\n')
+            answer = my_socket.recv(1024).decode('utf-8')
+
+    elif METHOD == 'INVITE':
+        templateSDP = "Content-Type: application/sdp\r\n\r\n" + "v=0\r\n" \
+                    + "o=" + str(config_data['account']['username']) + \
+                    " " + str(config_data['uaserver']['ip']) + \
+                    "\r\ns=LaMesa\r\n" + "t=0\r\nm=audio " + \
+                    str(config_data['rtpaudio']['puerto']) + " RTP\r\n\r\n"
+        line = METHOD + ' sip:' + OPTION + ' SIP/2.0\r\n' + templateSDP
         my_socket.send(bytes(line, 'utf-8'))
         answer = my_socket.recv(1024).decode('utf-8')
+        if '200' in answer:
+            line = 'ACK sip:' + OPTION + ' SIP/2.0\r\n'
+            my_socket.send(bytes(line, 'utf-8'))
     print("Fin.")
--- uaserver.py
+++ uaserver.py
@@ -10,8 +10,6 @@ import sys
 import os
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
-import hashlib
-
 
 
 class XmlHandler(ContentHandler):
@@ -23,7 +21,7 @@ class XmlHandler(ContentHandler):
     def startElement(self, name, attrs):
 
         posibatts = ['username', 'passwd', 'ip', 'puerto', 'path']
-        posibtags = ['account', 'uaserver', 'regproxy', 'rtpaudio', 
+        posibtags = ['account', 'uaserver', 'regproxy', 'rtpaudio',
                      'log', 'audio']
         for tag in posibtags:
             if name == tag:
@@ -36,35 +34,39 @@ class XmlHandler(ContentHandler):
     def get_tags(self):
         return self.tags
 
+
 class SIPServerHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
         line = self.rfile.read().decode('utf-8').split()
+        #print(line[0])
         my_methods = ['INVITE', 'ACK', 'BYE']
         if line[0] not in my_methods:
             self.wfile.write(b'SIP/2.0 405 Method Not Allowed\r\n\r\n')
-        elif len(line) != 3:
-            self.wfile.write(b'SIP/2.0 400 Bad Request\r\n\r\n')
+        #elif len(line) = 3:
+         #   self.wfile.write(b'SIP/2.0 400 Bad Request\r\n\r\n')
         else:
             if line[0] == 'INVITE':
+                print('llegaaaaa:', line)
                 templateSIP = ('SIP/2.0 100 Trying\r\n\r\n'
-                            'SIP/2.0 180 Ring\r\n\r\n'
-                            'SIP/2.0 200 OK\r\n\r\n')
-                self.wfile.write(bytes(templateSIP, 'utf-8'))
-                templateSDP = ("Content-Type: application/sdp\r\n\r\n" +
-                          "v=0\r\n" + "o=" + str(config_data['account']['username']) + 
-                          " " + str(config_data['uaserver']['ip']) + "\r\ns=LaMesa\r\n" +
-                          "t=0\r\nm=audio " + str(config_data['rtpaudio']['puerto']) +
-                          " RTP\r\n")
-                self.wfile.write(bytes(templateSDP, 'utf-8'))
+                               'SIP/2.0 180 Ring\r\n\r\n'
+                               'SIP/2.0 200 OK\r\n\r\n')
+                #self.wfile.write(bytes(templateSIP, 'utf-8'))
+                templateSDP = "Content-Type: application/sdp\r\n\r\n" + "v=0\r\n" \
+                    + "o=" + str(config_data['account']['username']) + \
+                    " " + str(config_data['uaserver']['ip']) + \
+                    "\r\ns=LaMesa\r\n" + "t=0\r\nm=audio " + \
+                    str(config_data['rtpaudio']['puerto']) + " RTP\r\n\r\n"
+                self.wfile.write(bytes(templateSIP + templateSDP, 'utf-8'))
             elif line[0] == 'ACK':
+                print('hola')
                 #os.system('./mp32rtp -i 127.0.0.1 -p 23032 < ' +
                  #         archivo_audio)
             elif line[0] == 'BYE':
                 self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
 
 if __name__ == "__main__":
-   
+
     try:
         CONFIG = sys.argv[1]
     except:
