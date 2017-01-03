37751ee7c257827976544f4c7b3adad49ae7bb45
Sergio Casarrubios <scasarrubios@virtualbox.org>
2017-01-03 04:06:08 +0100

    Añadidas funciones de control de datos

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -5,6 +5,7 @@ Programa servidor proxy SIP-SDP
 """
 
 import socketserver
+import socket
 import sys
 import time
 import json
@@ -12,7 +13,6 @@ import hashlib
 import random
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
-
 
 
 class ProxyXmlHandler(ContentHandler):
@@ -36,6 +36,7 @@ class ProxyXmlHandler(ContentHandler):
     def get_tags(self):
         return self.tags
 
+
 def get_psswd(user):
     try:
         file = open(str(config_data['database']['passwdpath']), "r")
@@ -49,27 +50,40 @@ def get_psswd(user):
         os.exit('ERROR: passwords file not found')
     return password
 
+
 class ProxyHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
     clients = {}
+    user = []
+    dest = []
     no_file = False
-    nonce = str(random.randint(000000000000000000000, 
+    nonce = str(random.randint(000000000000000000000,
                                99999999999999999999))
 
-    def caducity_check(self, line):
+    def caducity_check(self):
         """
         Compara la fecha de caducidad de los clientes del diccionario,
         y si han caducado los añade a la lista caduced para borrarlos
         """
         caduced = []
         for client in self.clients:
-            now = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+            now = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
             if now >= self.clients[client][2]:
                 caduced.append(client)
         for client in caduced:
             del self.clients[client]
+
+    def register_check(self, user):
+        """
+        Comprueba si la petición recibida es de un usuario registrado
+        """
+        registered = False
+        for client in self.clients:
+            if user == client:
+                registered = True
+        return registered
 
     def register2json(self):
         """
@@ -95,40 +109,63 @@ class ProxyHandler(socketserver.DatagramRequestHandler):
     def handle(self):
         self.json2register()
         line = self.rfile.read().decode('utf-8').split()
-        user = line[1].split(':')[1]
         print(line)
-        passwd = get_psswd(user)
-        print(passwd)
         print('PRIMERA')
         print(self.clients)
-        self.caducity_check(line)
-        if line[0] == 'REGISTER' and len(line) < 6:
+        self.caducity_check()
+        if line[0] == 'REGISTER' and len(line) < 6:  # Si falta autenticación
             to_send = 'SIP/2.0 401 Unauthorized\r\n' + \
                       'WWW Authenticate: Digest nonce="' + \
                       self.nonce + '"\r\n\r\n'
             self.wfile.write(bytes(to_send, 'utf-8'))
-        if line[0] == 'REGISTER' and len(line) >= 6:
+        elif line[0] == 'REGISTER' and len(line) >= 6:  # Autenticación recibida
+            user = line[1].split(':')[1]
             authenticate = hashlib.sha1()
-            authenticate.update(bytes(passwd,'utf-8'))
-            authenticate.update(bytes(self.nonce,'utf-8'))
+            authenticate.update(bytes(get_psswd(user), 'utf-8'))
+            authenticate.update(bytes(self.nonce, 'utf-8'))
             authenticate = authenticate.hexdigest()
             if authenticate == line[7].split('"')[1]:
                 print("PERFE")
                 data = []
-                data.append(self.client_address[0]) #añade la IP
-                data.append(line[1].split(':')[2]) #añade el puerto
-                self.clients[user] = data
+                data.append(self.client_address[0])  # Añade la IP
+                data.append(line[1].split(':')[2])  # Añade el puerto
                 if line[3] == 'Expires:' and line[4] == '0':
-                    del self.clients[user] 
+                    del self.clients[user]
                 elif line[3] == 'Expires:' and line[4] != '0':
-                    caduc_time = time.gmtime(time.time()+int(line[4]))
+                    caduc_time = time.localtime(time.time()+int(line[4]))
                     data.append(time.strftime('%Y-%m-%d %H:%M:%S', caduc_time))
                     self.clients[user] = data
                 self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
                 self.register2json()
                 print('SEGUNDA')
                 print(self.clients)
-            
+        elif line[0] == 'INVITE':
+            self.user.append(line[6][2:])
+            self.dest.append(line[1].split(':')[1])
+            registered = self.register_check(self.user[0])
+            user_found = self.register_check(self.dest[0])
+            if registered and user_found:
+                with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) \
+                as my_socket:  # Abrimos un socket con el cliente solicitado
+                    my_socket.connect((self.clients[self.dest[0]][0],
+                                       int(self.clients[self.dest[0]][1])))
+                    my_socket.send(bytes(' '.join(line), 'utf-8'))
+                    answer = my_socket.recv(1024).decode('utf-8')
+                    self.wfile.write(bytes(answer, 'utf-8'))
+            elif not registered:
+                self.wfile.write(bytes('401', 'utf-8'))
+            elif not user_found:
+                self.wfile.write(bytes('404', 'utf-8'))
+        elif line[0] == 'ACK':
+            registered = self.register_check(self.user[0])
+            if registered:
+                with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) \
+                as my_socket:  # Abrimos un socket con el cliente solicitado
+                    my_socket.connect((self.clients[self.dest[0]][0],
+                                       int(self.clients[self.dest[0]][1])))
+                    my_socket.send(bytes(' '.join(line), 'utf-8'))
+                self.user = []
+                self.dest = []
 if __name__ == "__main__":
     try:
         CONFIG = sys.argv[1]
@@ -140,7 +177,8 @@ if __name__ == "__main__":
     parser.setContentHandler(pxHandler)
     parser.parse(open(CONFIG))
     config_data = pxHandler.get_tags()
-    serv = socketserver.UDPServer(('', int(config_data['server']['puerto'])), ProxyHandler)
+    serv = socketserver.UDPServer(('', int(config_data['server']['puerto'])),
+                                  ProxyHandler)
     print('>> ' + config_data['server']['name'] + ' listening...\n')
     try:
         serv.serve_forever()
